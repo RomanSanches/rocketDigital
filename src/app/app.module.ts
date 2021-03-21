9ca8d80da4a00b73d194325aa7fec542b5b6badc
Roman <sanches.roman@gmail.com>
2021-03-21 12:03:07 +0100

    prueba tecnica Rocket Digital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { MenuComponent } from './menu/menu.component';
+import { HighlightedComponent } from './highlighted/highlighted.component';
+import { CardsComponent } from './cards/cards.component';
+import { LogoSlideComponent } from './logo-slide/logo-slide.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { RouterModule } from '@angular/router';
+import { BadgesComponent } from './badges/badges.component';
+import { WeeksComponent } from './weeks/weeks.component';
+import { BlogComponent } from './blog/blog.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    MenuComponent,
+    HighlightedComponent,
+    CardsComponent,
+    LogoSlideComponent,
+    BadgesComponent,
+    WeeksComponent,
+    BlogComponent,
+    SignUpComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    IvyCarouselModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/logo-slide/logo-slide.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-logo-slide',
+  templateUrl: './logo-slide.component.html',
+  styleUrls: ['./logo-slide.component.css']
+})
+export class LogoSlideComponent implements OnInit {
+
+
+  /* slidesStore = [{src:'https://s3-alpha-sig.figma.com/img/1c15/69f8/781159725448f277447e2572ae70a169?Expires=1616976000&Signature=EJPqV1Yp2yoRQMYBcWd4JwWLf-MpE6O45BY9qZ-bowRclDiuadAsmZNfXPNp~JfgoR5Ve7nLxXi26N9ku1kMZi35QBRGOiEC01-xFWxst4Xu4RK5FtTsI3NQlb1x3USqa0Od7s6mk-w1X-xlIR9EiNsqvCV-4F~NSna2SpARRxd54oRJEDnMHurbrYJPosruGIoLQ3jb7AwbfgUsZJy4YtAu1FpHwCk0yFuLyhwg2-YwEvSMhG79LgV4~JBy8cZXediWAk83o6m0sPlx5b9z~jeGGVGwuwetpbHCOdopxJ8T~~lGBQYJblN41et7mJ8S8bPkOCZXZsShacQd-yYpkQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},
+                  {src: 'https://s3-alpha-sig.figma.com/img/6e14/5383/f81936c25fd633d9ee9679bb9a8298a6?Expires=1616976000&Signature=B8KNfinq1vfJayI-WQiRiMnj1Tf-JcZrrAEPWIP5S0zR4Bg5~K9vqA97Ao8CeNI3H4lkRqpT-Fcizir1AViV6klegFLcwN32EmLZYXADtIIqcdsagVb6VKmhkc76PGDH831JihRMgwFHnledAP8EWW39VSNe9WltyX~i5uBFrovNHo9BWQUwsdJSBa2qJatEu6mGik7f6QuRdme1FHu3vCm2M0ujnH9Bx88yy0ESqP2lHK9Opku5xG4Of0mU421Y48RX7Rx6PT1vK-~SKvzUE1WPjnUTdSABH2dSlio-wrBLBjFbHY63d38nvKQU2CF7VeRdTg0qvko~-jRVsYlr2Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'},
+                  {src: 'https://s3-alpha-sig.figma.com/img/a8cc/848b/012a54ef4210573df021de9d7792c7d1?Expires=1616976000&Signature=fi2hD3k2D74REike2C-7aSGp6bsAt-PxqNOpFsqbwUhncbKBeeGkfI4PR9v2dxEtznz9EHIM2KT-2hG3BLtlRB8zgrHAQsWBV26bhnHYsrl2mPnXpHS98maFyKzuuWy1A6ebFu5T36uozkG~Am4Ye8PrBQeYnEhSd~q3DY8hXkTDd6ZhZpdui-C2FnOAh~xkvhYuEEUHsSQMX6Q1Q1NwBJgdyrlByq8NtdsNQdXLXGtyt~ZP8UU0oXyuRoQzDqk3QbLiL3uGBdneFvbiDfyyFGnWg3g3hz4eQtcaegE-ntPpjT15te605zTk7JrNlouyNq6-SRUqgq~8nkrB~U6lvg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'}]
+ */
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+
+
+}
